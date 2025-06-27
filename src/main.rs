93a20a807ef3e6f4bd69aaa73c2515ff3fec79f9
Marc Durham <marcdurham@gmail.com>
2025-06-26 23:34:46 -0700

    Debugging SMS

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,15 @@ async fn send_sms(
     args: &Args,
     message: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    // Debug print all available arguments
+    println!("SMS Function Args Debug:");
+    println!("  sms_api_username: {:?}", args.sms_api_username);
+    println!("  sms_api_password: {:?}", args.sms_api_password.as_ref().map(|_| "[REDACTED]"));
+    println!("  sms_from_phone_number: {:?}", args.sms_from_phone_number);
+    println!("  sms_to_phone_number: {:?}", args.sms_to_phone_number);
+    println!("  message: {:?}", message);
 
 
-    
     if let (Some(username), Some(password), Some(from), Some(to)) = (
         &args.sms_api_username,
         &args.sms_api_password,
@@ -68,6 +74,8 @@ async fn send_sms(
         println!("Voip URI: {}", uri);
 
         let response = client.get(&uri).send().await?;
+
+        println!("SMS Response: {}", response.status().as_str());
         
         if response.status().is_success() {
             println!("SMS sent successfully: {}", message);
