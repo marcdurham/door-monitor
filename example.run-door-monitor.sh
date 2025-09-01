a18bbceb5c2516568b861adfa6a78ca687f1c9ce
Marc Durham <marcdurham@gmail.com>
2025-09-01 16:47:07 -0700

    Update the example

--- example.run-door-monitor.sh
+++ example.run-door-monitor.sh
@@ -6,4 +6,7 @@ cargo run -- \
   --sms-from-phone-number 2065551111 \
   --api-url http://192.168.1.226/rpc/Input.GetStatus\?id\=0 \
   --check-interval-seconds 5 \
-  --open-too-long-seconds 300
+  --open-too-long-seconds 300 \
+  --telegram-token "1111111111:AAAAAAAAAAAAAA" \
+  --telegram-conversation-id 99999999999
+
